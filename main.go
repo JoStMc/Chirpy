8c4d56b09a2e5321153d44406778a6d00e0acf54
JoStMc <joshmcdonnell@hotmail.co.uk>
2026-02-20 22:28:10 +0000

    app and healthz handlers

--- main.go
+++ main.go
@@ -7,10 +7,20 @@ import (
 
 func main() {
 	mux := http.NewServeMux()
+	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir("."))))
+	mux.HandleFunc("/healthz", handlerReadiness)
+
 	server := http.Server{
 	    Handler: mux,
 		Addr: ":8080",
 	} 
 	log.Println("Serving on port 8080")
+
 	log.Fatal(server.ListenAndServe())
+}
+
+func handlerReadiness(w http.ResponseWriter, req *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(http.StatusText(http.StatusOK)))
 }
