317c0f3fe0de564045bf488f1b9716ddcf542bd5
JoStMc <joshmcdonnell@hotmail.co.uk>
2026-02-22 21:41:55 +0000

    added postgres integration

--- main.go
+++ main.go
@@ -1,18 +1,34 @@
 package main
 
 import (
+	"database/sql"
 	"log"
 	"net/http"
+	"os"
 	"sync/atomic"
+
+	"github.com/JoStMc/Chirpy/internal/database"
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
 )
 
 type apiConfig struct {
 	fileserverHits atomic.Int32
+	dbQueries *database.Queries
 } 
 
 func main() {
+	if err := godotenv.Load(); err != nil {
+		log.Fatal("unable to load env:", err)
+	} 
+	db, err := sql.Open("postgres", os.Getenv("DB_URL"))
+	if err != nil {
+		log.Fatal("unable to open db:", err)
+	} 
+
 	cfg := apiConfig{
 		fileserverHits: atomic.Int32{},
+		dbQueries: database.New(db),
 	}
 
 	mux := http.NewServeMux()
